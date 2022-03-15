32fc1811007e5aaf5e77f402b7878a8c9a6d13dc
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-03-15 18:01:15 +0200

    Update API_curl.py

--- source_code/API_curl.py
+++ source_code/API_curl.py
@@ -8,11 +8,12 @@ f = open('data.json', "r")
 #  Load data from json file to data variable.
 #  Because there are multiple items the data type is a list.
 #  When there is only one item it usually will be a dict
+
 data = json.load(f)
 
 for i in data:
-    print("Title: ", ____)
-    print("Body: ", ____)
+    print("Title: ", i["title"])
+    print("Body: ", i["body"])
 
 
 # for another point of view
