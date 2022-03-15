b63cc89f5255ad03e782779e34931598363555bb
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-03-15 18:02:42 +0200

    Update API_requests.py

--- source_code/API_requests.py
+++ source_code/API_requests.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+"""
 # GET method to fetch data
 url = 'https://jsonplaceholder.typicode.com/posts'
 
@@ -11,25 +12,31 @@ r = requests.get(url)
 data = r.json()
 
 for i in data:
-    print("Title: ", ____)
-    print("Body: ", ____)
+    print("Title: ", i['title'])
+    print("Body: ", i['body'])
 
 
 # print(json.dumps(data, indent=4))
 print("Return of requests.get when fetching many files:", type(data))
 print("Return type of requests.get when fetching many files: ", type(data[0]))
 
+"""
+
+"""
+
 # Same as before but with only one record
 url_1 = 'https://jsonplaceholder.typicode.com/posts/1'
 
-r_1 = ____
-data_1 = ____
+r_1 = requests.get(url_1)
+data_1 = data = r_1.json()
 
 print(data_1)
 
-# Now data is a dict because there aren't any other reocrds
+# Now data is a dict because there aren't any other records
 print("Return of requests.get when fetching a single file:", type(data_1))
 print("Dict data from single file", data_1)
+
+"""
 
 #  POST method to upload data
 
@@ -38,7 +45,7 @@ task = {'userId': 1, 'id': 1, 'title': 'foo', 'body': 'bar'}
 task_json = json.dumps(task) # Optional, good practice
 
 # Use the correct requests function to call the POST method for the json
-resp = requests.____('https://jsonplaceholder.typicode.com/posts', json=task)
+resp = requests.post('https://jsonplaceholder.typicode.com/posts', json=task)
 
 # Print response number. 201 => success
 print(resp)
